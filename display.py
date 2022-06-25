825ae3b3448ae46867c88066633f0def59c198d3
JJEMI11S <42531494+JJEMI11S@users.noreply.github.com>
2022-06-26 02:36:55 +0800

    Add files via upload
    changed style so it no longer acts as an overlay for Spotify but as an extensional window triggered by a small button placed above my Spotify app on my taskbar. This both windows stay above all other apps and windows and the lyrics are draggable

--- display.py
+++ display.py
@@ -6,27 +6,27 @@ from tkinter import messagebox
 
 
 # get position of spotify window on screen
-position = pyautogui.locateCenterOnScreen("search.png")
-while position == None:
-    position = pyautogui.locateCenterOnScreen("useless.png")
-
+# position = pyautogui.locateCenterOnScreen("search.png")
+# # while position == None:
+# #     position = pyautogui.locateCenterOnScreen("Capture.png")
+# print(position, "yoy")
 
 class App():
     def __init__(self):
         self.root = Tk()
-        self.root.geometry('%dx%d+%d+%d' % (390, 50, position.x - 88, position.y - 15))
+        self.root.geometry('%dx%d+%d+%d' % (25, 25, 694, 1005))
         self.root.overrideredirect(1)
+        self.root.attributes("-topmost", True)
         self.img = PhotoImage(file="cover.png")
-        self.frame = Frame(self.root, width=390, height=50, bg="black",
+        self.img2 = PhotoImage(file="lyricalgenius.png")
+        self.frame = Frame(self.root, width=25, height=50, bg="black",
                            borderwidth=0, relief=RAISED)
         self.frame.pack_propagate(False)
         self.frame.grid()
         self.toggle_lyrics = False
-        self.background = Label(self.frame, image=self.img, bg="black")
-        self.background.grid(column=1, row=1)
-        self.bQuit = Button(self.frame, text="Lyrics", fg="white",bg="black", borderwidth=0,
+        self.blyrics = Button(self.frame, image=self.img2, fg="white",bg="black", borderwidth=0,
                             command=self.lyrics_show)
-        self.bQuit.grid(column=2, row=1, sticky="N")
+        self.blyrics.grid(column=2, row=1, sticky="N")
         # photo = PhotoImage(file="lyricalgenius.png")
         #
         # self.bHello = Button(self.frame, text="Lyrics",fg="Grey",
@@ -35,24 +35,57 @@ class App():
 
     def lyrics_show(self):
         if self.toggle_lyrics == False:
+
             self.toggle_lyrics = True
             lyrics = lyriccheck()
             self.newWindow = Toplevel(self.root)
             self.newWindow.title("New Window")
-            self.newWindow.geometry('%dx%d+%d+%d' % (480, 833, position.x-88, position.y+20))
+            self.newWindow.geometry('%dx%d+%d+%d' % (500, 300, 1400, 700))
             self.newWindow.overrideredirect(1)
-            frame = Frame(self.newWindow, width=480, height=833, bg="black",
+            self.newWindow.attributes("-topmost", True)
+            frame = Frame(self.newWindow, width=500, height=300, bg="black",
                         borderwidth=0, relief=RAISED)
             frame.pack_propagate(False)
             frame.pack()
-            T = Text(frame, height=833, width =480, font=("Gotham-Bold", 12), bg="black", fg="white")
-            T.pack(side=LEFT)
+            grip = Label(frame, bitmap="gray25")
+            grip.pack(side=TOP)
+            T = Text(frame, height=833, width =500, font=("Gotham-Bold", 12), bg="black", fg="white")
+
+
             T.insert(END,lyrics)
+            T.tag_add("center", "1.0", "end")
+            T.pack(side=LEFT)
             scroll_bar = Scrollbar(frame,
                                    orient=VERTICAL).pack(side=RIGHT,fill=Y)
+            self.label = Label(frame, text="Click on the grip to move")
+
+            self.label.pack(side="right", fill="both", expand=True)
+
+            grip.bind("<ButtonPress-1>", self.start_move)
+            grip.bind("<ButtonRelease-1>", self.stop_move)
+            grip.bind("<B1-Motion>", self.do_move)
+
         else:
             self.newWindow.destroy()
             self.toggle_lyrics = False
+
+
+    def start_move(self, event):
+        self.x = event.x
+        self.y = event.y
+
+
+    def stop_move(self, event):
+        self.x = None
+        self.y = None
+
+
+    def do_move(self, event):
+        deltax = event.x - self.x
+        deltay = event.y - self.y
+        x = self.newWindow.winfo_x() + deltax
+        y = self.newWindow.winfo_y() + deltay
+        self.newWindow.geometry(f"+{x}+{y}")
 
     def hide(self):
         self.root.withdraw()
@@ -60,6 +93,8 @@ class App():
     def show(self):
         self.root.update()
         self.root.deiconify()
+
+
 
 app = App()
 app.root.mainloop()
